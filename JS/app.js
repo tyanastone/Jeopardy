67ab8fa068fcbcd87dd6b9de2a623663c4d684a7
Tyana <tyana.stone@gmail.com>
2018-10-12 16:19:06 -0400

    tried to test a function to check for right answer

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+// var first= querySelector()
+// var score = 0
+// $('input[type=radio]').click(function () {
+   
+//     points += 10    
+//     .innerHTML =  
+// });
+
+// var score = document.querySelector("#score")
+// var changeScore = 0
+
+// function answer(a, b) {
+// if ()
+
+
+// }
+
+var score=0;
+
+function checkAll() {
+
+    function questioncheckOne() {
+        var correctAnswer = document.getElementById("female")
+        if (correctAnswer.checked == true) {
+            score++;
+            alert("Correct, your score is now " + score)
+        }
+        else {
+            alert("Wrong, your score is now " + score)
+        }
+    };
+questioncheckOne()
+}
